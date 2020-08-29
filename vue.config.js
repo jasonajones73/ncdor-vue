bf5712fbd6a14c9e4c73f6e49115c0262e2e8669
Jason Jones <jasonajones73@gmail.com>
2020-08-29 16:50:51 -0400

    fixing path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: "https://jasonajones73.github.io/ncdor-vue/",
-  baseUrl: "/ncdor-vue/"
+  publicPath: "https://jasonajones73.github.io/ncdor-vue/"
 };
