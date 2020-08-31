b4bc33b76943d58d670ac1647f216a8a4827ed28
Jason Jones <jasonajones73@gmail.com>
2020-08-31 10:45:13 -0400

    adding one line for About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,9 @@
         About
       </v-card-title>
       <v-card-text>
-        This is where information about the site would be inserted.
+        This site focuses on the simplified distribution of North Carolina
+        Department of Revenue (NC DOR) local government sales tax data. More
+        details for this project coming soon!
       </v-card-text>
     </v-card>
   </v-container>
